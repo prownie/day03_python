f67204395fb3969874ab25dd364d12986fe4266d
rpichon <pichon.robin1@gmail.com>
2020-05-28 15:11:50 +0200

    push ex01

--- ex01/ImageProcessor.py
+++ ex01/ImageProcessor.py
@@ -1,2 +1,37 @@
 import numpy
-import matplotlib.pyplot as plt
+from PIL import Image
+
+
+class ImageProcessor:
+
+    def __init__(self):
+        pass
+
+    def load(self, path):
+        try:
+            img = numpy.array(Image.open(path))
+            width = numpy.size(img, 1)
+            height = numpy.size(img, 0)
+            print("Loading image of dimensions {} x {}".format(width, height))
+            return(img)
+        except AttributeError:
+            print("Error in loading file, wrong path")
+            return(None)
+        except FileNotFoundError:
+            print("Error in loading file, wrong path")
+            return(None)
+        except:
+            print("Something Wrong Happened")
+            return(None)
+
+    def display(self, array):
+        try:
+            to_disp = Image.fromarray(array, 'RGBA')
+            to_disp.show()
+        except:
+            print("Can't display this")
+
+
+imp = ImageProcessor()
+arr = imp.load("rickd.png")
+imp.display(arr)
